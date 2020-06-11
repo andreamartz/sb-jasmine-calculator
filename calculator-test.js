d86991288beb1d37e1781b71af5ec729b6e8d635
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-11 14:00:11 -0500

    Write tests for calculator app and verify passing

--- calculator-test.js
+++ calculator-test.js
@@ -1,11 +1,28 @@
+describe("calculateMontlyPayment(values) tests", function () {
+  it("should calculate the monthly rate correctly", function () {
+    expect(
+      calculateMonthlyPayment({
+        amount: 0,
+        years: 2.5,
+        rate: 3.2575,
+      })
+    ).toEqual("0.00");
+    expect(
+      calculateMonthlyPayment({
+        amount: 5000,
+        years: 2.5,
+        rate: 10.999999,
+      })
+    ).toEqual("191.39");
+  });
 
-it('should calculate the monthly rate correctly', function () {
-  // ...
+  it("should return a result with 2 decimal places", function () {
+    expect(
+      calculateMonthlyPayment({
+        amount: 10000,
+        years: 2.5,
+        rate: 3.2575,
+      })
+    ).toEqual("347.54");
+  });
 });
-
-
-it("should return a result with 2 decimal places", function() {
-  // ..
-});
-
-/// etc
