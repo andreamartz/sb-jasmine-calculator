28bcd07c86d1f773858b86c4b01c7c3404f1970d
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-11 12:56:28 -0500

    Refactor code to simplify & clarify

--- calculator.js
+++ calculator.js
@@ -19,31 +19,30 @@ function getCurrentUIValues() {
 
 function setupIntialValues() {
   // Get the inputs from the DOM.
-  const form = document.getElementById("calc-form");
-  const amountInput = form.querySelector("#loan-amount");
-  const yearsInput = form.querySelector("#loan-years");
-  const rateInput = form.querySelector("#loan-rate");
+  const amountUI = document.querySelector("#loan-amount");
+  const yearsUI = document.querySelector("#loan-years");
+  const rateUI = document.querySelector("#loan-rate");
 
   // Put some default values in the inputs
-  amountInput.value = 10000;
-  yearsInput.value = 2.5;
-  rateInput.value = 3.2575;
-
   const values = {
-    amount: amountInput,
-    years: yearsInput,
-    rate: rateInput,
+    amount: 10000,
+    years: 2.5,
+    rate: 3.2575,
   };
 
+  amountUI.value = values.amount;
+  yearsUI.value = values.years;
+  rateUI.value = values.rate;
+
   // Call a function to calculate the current monthly payment
-  calculateMonthlyPayment(values);
+  update();
 }
 
 function update() {
   // Get the current values from the UI
   const values = getCurrentUIValues();
 
-  // Update the monthly payment
+  // Update the monthly payment calculation and
   const payment = calculateMonthlyPayment(values);
   updateMonthly(payment);
 }
@@ -51,11 +50,11 @@ function update() {
 function calculateMonthlyPayment(values) {
   // Given an object of values (a value has amount, years and rate), calculate the monthly payment.
   const { amount, years, rate } = values;
-  const monthlyRateDecimal = rate / 12 / 100;
-  const months = years * 12;
+  const int = rate / 12 / 100;
+  const n = years * 12;
 
-  const monthlyPmtNumerator = amount * monthlyRateDecimal;
-  const monthlyPmtDenominator = 1 - Math.pow(1 + monthlyRateDecimal, -months);
+  const monthlyPmtNumerator = amount * int;
+  const monthlyPmtDenominator = 1 - Math.pow(1 + int, -n);
   const monthlyPmt = monthlyPmtNumerator / monthlyPmtDenominator;
   // The output should be a string that always has 2 decimal places.
   return String(monthlyPmt.toFixed(2));
