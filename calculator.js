6b2e1e71138cea6ac23f12000ec7bac9f82c2376
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-06-11 10:00:35 -0500

    Complete setupInitialValues fcn

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("calc-form");
   if (form) {
     setupIntialValues();
-    form.addEventListener("submit", function(e) {
+    form.addEventListener("submit", function (e) {
       e.preventDefault();
       update();
     });
@@ -11,30 +11,45 @@ window.addEventListener('DOMContentLoaded', function() {
 
 function getCurrentUIValues() {
   return {
-    amount: +(document.getElementById("loan-amount").value),
-    years: +(document.getElementById("loan-years").value),
-    rate: +(document.getElementById("loan-rate").value),
-  }
+    amount: +document.getElementById("loan-amount").value,
+    years: +document.getElementById("loan-years").value,
+    rate: +document.getElementById("loan-rate").value,
+  };
 }
 
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  // Get the inputs from the DOM.
+  const form = document.getElementById("calc-form");
+  const formInputs = Array.from(form.querySelectorAll("input"));
+  const amountInput = form.querySelector("#loan-amount");
+  const yearsInput = form.querySelector("#loan-years");
+  const rateInput = form.querySelector("#loan-rate");
+  console.log(amountInput, yearsInput, rateInput);
+
+  // Put some default values in the inputs
+  amountInput.value = 10000;
+  yearsInput.value = 2.5;
+  rateInput.value = 3.2575;
+
+  const values = {
+    amountInput: amountInput,
+    yearsInput: yearsInput,
+    rateInput: rateInput,
+  };
+  console.log("values: ", values);
+  // Call a function to calculate the current monthly payment
+  calculateMonthlyPayment(values);
 }
 
 // Get the current values from the UI
 // Update the monthly payment
-function update() {
-}
+function update() {}
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
-}
+function calculateMonthlyPayment(values) {}
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
-function updateMonthly(monthly) {
-}
+function updateMonthly(monthly) {}
